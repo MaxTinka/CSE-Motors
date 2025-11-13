5a21105ab6755a6920c1ab837fc46f2d4fa20ff9
MaxTinka <tinkamax03@gmail.com.com>
2025-11-13 03:16:56 +0300

    week2

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5500;
+
+// Import routes
+const inventoryRoutes = require('./routes/inventoryRoutes');
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// MVC Routes
+app.use('/inventory', inventoryRoutes);
+
+// Home route
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'CSE Motors Uganda - Quality Vehicles',
@@ -21,6 +30,23 @@ app.get('/', (req, res) => {
             month: 'long',
             day: 'numeric'
         })
+    });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).render('error', {
+        title: 'Server Error - CSE Motors',
+        message: 'Something went wrong!'
+    });
+});
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).render('error', {
+        title: 'Page Not Found - CSE Motors',
+        message: 'The page you are looking for does not exist.'
     });
 });
 
