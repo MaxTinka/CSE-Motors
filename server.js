f9ec189d0a3f62b35c26cb2a54cfbd37b6a32554
MaxTinka <tinkamax03@gmail.com>
2025-11-20 21:23:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -27,17 +27,16 @@ app.use(static)
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
-
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 10000
-const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 10000;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, host, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
+
