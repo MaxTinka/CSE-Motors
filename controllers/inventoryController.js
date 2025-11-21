bbd8dfaef74a84989a3394bf115ec7bf9bdc6219
MaxTinka <tinkamax03@gmail.com>
2025-11-22 00:06:36 +0300

    Refactor inventory controller methods for clarity

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,20 +1,37 @@
-const invModel = require("../models/inventory-model");
-const utilities = require("../utilities");
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
 
-const invController = {};
+const invController = {}
 
-// Build inventory by classification view
+/* ***************************
+ *  Build inventory by classification view
+ * ************************** */
 invController.buildByClassificationId = async function (req, res, next) {
-  // Your code here
-};
+  const classification_id = req.params.classificationId
+  let data = await invModel.getInventoryByClassificationId(classification_id)
+  const grid = await utilities.buildClassificationGrid(data)
+  let nav = await utilities.getNav()
+  const className = data[0].classification_name
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  })
+}
 
-// Build vehicle detail view
+/* ***************************
+ *  Build vehicle detail view
+ * ************************** */
 invController.buildByInvId = async function (req, res, next) {
-  const inv_id = req.params.invId;
-  const data = await invModel.getInventoryById(inv_id);
-  const grid = await utilities.buildVehicleDetail(data);
+  const inv_id = req.params.invId
+  let data = await invModel.getInventoryById(inv_id)
+  const grid = await utilities.buildVehicleDetail(data)
+  let nav = await utilities.getNav()
   res.render("./inventory/detail", {
     title: data.inv_make + " " + data.inv_model,
+    nav,
     grid,
-  });
-};
+  })
+}
+
+module.exports = invController
