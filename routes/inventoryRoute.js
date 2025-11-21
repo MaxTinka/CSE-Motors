46585103550fc057983e5fcc8b8779aaa79cedb7
MaxTinka <tinkamax03@gmail.com>
2025-11-22 00:07:04 +0300

    Format inventoryRoute.js for consistency

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const router = express.Router();
-const invController = require("../controllers/inventoryController");
+const express = require("express")
+const router = express.Router()
+const invController = require("../controllers/inventoryController")
 
 // GET inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", invController.buildByClassificationId)
 
 // GET vehicle detail view
-router.get("/detail/:invId", invController.buildByInvId);
+router.get("/detail/:invId", invController.buildByInvId)
 
-module.exports = router;
+module.exports = router
