8b4f384a384cb5663f0abcf0fb2a38fe2ae2ff6b
MaxTinka <tinkamax03@gmail.com>
2025-11-22 00:24:30 +0300

    Update inventory-model.js

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,24 +1,42 @@
 const pool = require("../database/")
 
+const invModel = {}
+
+/* ***************************
+ *  Get all classifications
+ * ************************** */
+invModel.getClassifications = async function () {
+  try {
+    const data = await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+    return data
+  } catch (error) {
+    console.error("getClassifications error: " + error)
+    return { rows: [] }
+  }
+}
+
 /* ***************************
  *  Get inventory by classification_id
  * ************************** */
-async function getInventoryByClassificationId(classification_id) {
+invModel.getInventoryByClassificationId = async function (classification_id) {
   try {
     const data = await pool.query(
-      "SELECT * FROM public.inventory AS i JOIN public.classification AS c ON i.classification_id = c.classification_id WHERE i.classification_id = $1",
+      `SELECT * FROM public.inventory AS i 
+       JOIN public.classification AS c ON i.classification_id = c.classification_id 
+       WHERE i.classification_id = $1`,
       [classification_id]
     )
     return data.rows
   } catch (error) {
-    console.error("getclassificationsbyid error " + error)
+    console.error("getInventoryByClassificationId error: " + error)
+    return []
   }
 }
 
 /* ***************************
  *  Get inventory item by inv_id
  * ************************** */
-async function getInventoryById(inv_id) {
+invModel.getInventoryById = async function (inv_id) {
   try {
     const data = await pool.query(
       "SELECT * FROM public.inventory WHERE inv_id = $1",
@@ -27,7 +45,8 @@ async function getInventoryById(inv_id) {
     return data.rows[0]
   } catch (error) {
     console.error("getInventoryById error: " + error)
+    return null
   }
 }
 
-module.exports = { getInventoryByClassificationId, getInventoryById }
+module.exports = invModel
