42ece78dd71e23ee9139d8810cee39f897c93d93
MaxTinka <tinkamax03@gmail.com>
2025-11-22 00:05:52 +0300

    Refactor inventory model functions for clarity

--- models/inventory-models.js
+++ models/inventory-models.js
@@ -1,14 +1,33 @@
-const pool = require("../database/");
+const pool = require("../database/")
 
-// Get vehicle by ID
-inventoryModel.getInventoryById = async function (inv_id) {
+/* ***************************
+ *  Get inventory by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+  try {
+    const data = await pool.query(
+      "SELECT * FROM public.inventory AS i JOIN public.classification AS c ON i.classification_id = c.classification_id WHERE i.classification_id = $1",
+      [classification_id]
+    )
+    return data.rows
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error)
+  }
+}
+
+/* ***************************
+ *  Get inventory item by inv_id
+ * ************************** */
+async function getInventoryById(inv_id) {
   try {
     const data = await pool.query(
       "SELECT * FROM public.inventory WHERE inv_id = $1",
       [inv_id]
-    );
-    return data.rows[0];
+    )
+    return data.rows[0]
   } catch (error) {
-    console.error("getInventoryById error: " + error);
+    console.error("getInventoryById error: " + error)
   }
-};
+}
+
+module.exports = { getInventoryByClassificationId, getInventoryById }
