df78163382129540fbe80c8cbd2d7c34e1736a2f
MaxTinka <tinkamax03@gmail.com>
2025-11-21 23:58:35 +0300

    Add inventory routes for classification and detail views

---
+++ routes/inventoryRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const invController = require("../controllers/inventoryController");
+
+// GET inventory by classification view
+router.get("/type/:classificationId", invController.buildByClassificationId);
+
+// GET vehicle detail view
+router.get("/detail/:invId", invController.buildByInvId);
+
+module.exports = router;
