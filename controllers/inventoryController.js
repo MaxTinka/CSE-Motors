085ffcf024d3ef764bd5f0fc2fc29af54436d05b
MaxTinka <tinkamax03@gmail.com>
2025-11-22 00:35:21 +0300

    Update inventoryController.js

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -9,6 +9,18 @@ const invController = {}
 invController.buildByClassificationId = async function (req, res, next) {
   const classification_id = req.params.classificationId
   let data = await invModel.getInventoryByClassificationId(classification_id)
+  
+  if (!data || data.length === 0) {
+    // Handle no vehicles found
+    let nav = await utilities.getNav()
+    res.render("./inventory/classification", {
+      title: "No Vehicles Found",
+      nav,
+      grid: '<p class="notice">Sorry, no matching vehicles could be found.</p>',
+    })
+    return
+  }
+  
   const grid = await utilities.buildClassificationGrid(data)
   let nav = await utilities.getNav()
   const className = data[0].classification_name
@@ -25,6 +37,18 @@ invController.buildByClassificationId = async function (req, res, next) {
 invController.buildByInvId = async function (req, res, next) {
   const inv_id = req.params.invId
   let data = await invModel.getInventoryById(inv_id)
+  
+  // ADD THIS NULL CHECK - FIXES THE ERROR
+  if (!data) {
+    let nav = await utilities.getNav()
+    res.render("./inventory/detail", {
+      title: "Vehicle Not Found",
+      nav,
+      grid: '<p class="notice">Sorry, vehicle details could not be found.</p>',
+    })
+    return
+  }
+  
   const grid = await utilities.buildVehicleDetail(data)
   let nav = await utilities.getNav()
   res.render("./inventory/detail", {
