f7fa1e904d2c1fb023652a75062c560eb392b40b
MaxTinka <tinkamax03@gmail.com>
2025-11-22 00:25:07 +0300

    Update index.js

--- utilities/index.js
+++ utilities/index.js
@@ -6,31 +6,42 @@ const utilities = {}
  * Construct navigation menu
  * ************************** */
 utilities.getNav = async function () {
-  let data = await invModel.getClassifications()
-  let list = "<ul>"
-  list += '<li><a href="/" title="Home page">Home</a></li>'
-  data.rows.forEach((row) => {
-    list += "<li>"
-    list +=
-      '<a href="/inv/type/' +
-      row.classification_id +
-      '" title="See our inventory of ' +
-      row.classification_name +
-      ' vehicles">' +
-      row.classification_name +
-      "</a>"
-    list += "</li>"
-  })
-  list += "</ul>"
-  return list
+  try {
+    let data = await invModel.getClassifications()
+    let list = '<ul>'
+    list += '<li><a href="/" title="Home page">Home</a></li>'
+    
+    // Check if data and data.rows exist
+    if (data && data.rows) {
+      data.rows.forEach((row) => {
+        list += '<li>'
+        list +=
+          '<a href="/inv/type/' +
+          row.classification_id +
+          '" title="See our inventory of ' +
+          row.classification_name +
+          ' vehicles">' +
+          row.classification_name +
+          '</a>'
+        list += '</li>'
+      })
+    }
+    
+    list += '</ul>'
+    return list
+  } catch (error) {
+    console.error("getNav error: " + error)
+    // Return basic navigation if there's an error
+    return '<ul><li><a href="/">Home</a></li></ul>'
+  }
 }
 
 /* ***************************
  * Build the classification view HTML
  * ************************** */
 utilities.buildClassificationGrid = async function (data) {
-  let grid
-  if (data.length > 0) {
+  let grid = ''
+  if (data && data.length > 0) {
     grid = '<ul id="inv-display">'
     data.forEach((vehicle) => {
       grid += '<li>'
@@ -39,36 +50,36 @@ utilities.buildClassificationGrid = async function (data) {
         vehicle.inv_id +
         '" title="View ' +
         vehicle.inv_make +
-        " " +
+        ' ' +
         vehicle.inv_model +
         ' details"><img src="' +
         vehicle.inv_thumbnail +
         '" alt="Image of ' +
         vehicle.inv_make +
-        " " +
+        ' ' +
         vehicle.inv_model +
         ' on CSE Motors" /></a>'
       grid += '<div class="namePrice">'
-      grid += "<hr />"
-      grid += "<h2>"
+      grid += '<hr />'
+      grid += '<h2>'
       grid +=
         '<a href="../../inv/detail/' +
         vehicle.inv_id +
         '" title="View ' +
         vehicle.inv_make +
-        " " +
+        ' ' +
         vehicle.inv_model +
         ' details">' +
         vehicle.inv_make +
-        " " +
+        ' ' +
         vehicle.inv_model +
-        "</a>"
-      grid += "</h2>"
-      grid += "<span>$" + new Intl.NumberFormat().format(vehicle.inv_price) + "</span>"
-      grid += "</div>"
-      grid += "</li>"
+        '</a>'
+      grid += '</h2>'
+      grid += '<span>$' + new Intl.NumberFormat().format(vehicle.inv_price) + '</span>'
+      grid += '</div>'
+      grid += '</li>'
     })
-    grid += "</ul>"
+    grid += '</ul>'
   } else {
     grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
   }
