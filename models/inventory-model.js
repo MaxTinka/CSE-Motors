52dd29393019c51c033bb8fee88f1f7707315807
MaxTinka <tinkamax03@gmail.com>
2025-11-25 21:09:45 +0300

    Add functions to add classifications and inventory items

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -2,49 +2,46 @@ const pool = require("../database/")
 
 const invModel = {}
 
+// ... keep your existing functions ...
+
 /* ***************************
- *  Get all classifications
+ *  Add new classification
  * ************************** */
-invModel.getClassifications = async function () {
+invModel.addClassification = async function (classification_name) {
   try {
-    const data = await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
-    return data
+    const sql = "INSERT INTO classification (classification_name) VALUES ($1) RETURNING *"
+    return await pool.query(sql, [classification_name])
   } catch (error) {
-    console.error("getClassifications error: " + error)
-    return { rows: [] }
+    console.error("addClassification error: " + error)
+    return null
   }
 }
 
 /* ***************************
- *  Get inventory by classification_id
+ *  Add new inventory item
  * ************************** */
-invModel.getInventoryByClassificationId = async function (classification_id) {
+invModel.addInventory = async function (inventoryData) {
   try {
-    const data = await pool.query(
-      `SELECT * FROM public.inventory AS i 
-       JOIN public.classification AS c ON i.classification_id = c.classification_id 
-       WHERE i.classification_id = $1`,
-      [classification_id]
-    )
-    return data.rows
+    const sql = `INSERT INTO inventory (
+      inv_make, inv_model, inv_year, inv_description, 
+      inv_image, inv_thumbnail, inv_price, inv_miles, 
+      inv_color, classification_id
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`
+    
+    return await pool.query(sql, [
+      inventoryData.inv_make,
+      inventoryData.inv_model, 
+      inventoryData.inv_year,
+      inventoryData.inv_description,
+      inventoryData.inv_image,
+      inventoryData.inv_thumbnail,
+      inventoryData.inv_price,
+      inventoryData.inv_miles,
+      inventoryData.inv_color,
+      inventoryData.classification_id
+    ])
   } catch (error) {
-    console.error("getInventoryByClassificationId error: " + error)
-    return []
-  }
-}
-
-/* ***************************
- *  Get inventory item by inv_id
- * ************************** */
-invModel.getInventoryById = async function (inv_id) {
-  try {
-    const data = await pool.query(
-      "SELECT * FROM public.inventory WHERE inv_id = $1",
-      [inv_id]
-    )
-    return data.rows[0]
-  } catch (error) {
-    console.error("getInventoryById error: " + error)
+    console.error("addInventory error: " + error)
     return null
   }
 }
