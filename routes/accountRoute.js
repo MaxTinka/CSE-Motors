fe034500ae61288dcb7210da4e3f9ea6da2298ed
MaxTinka <tinkamax03@gmail.com>
2025-12-02 23:39:09 +0300

    Refactor account routes to remove auth middleware
    Removed authentication middleware from protected routes and added validation for update and password change routes.

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const accountController = require("../controllers/accountController");
-const authMiddleware = require("../utilities/authMiddleware");
 
 // Public routes
 router.get("/login", accountController.buildLogin);
@@ -9,22 +8,11 @@ router.get("/register", accountController.buildRegister);
 router.post("/register", accountController.registerAccount);
 router.post("/login", accountController.accountLogin);
 
-// Protected routes
-router.get("/", authMiddleware.requireLogin, accountController.buildAccountManagement);
-router.get("/update", authMiddleware.requireLogin, accountController.buildUpdateAccount);
-
-// Update routes with validation middleware
-router.post("/update", 
-  authMiddleware.requireLogin,
-  accountController.validateUpdate,
-  accountController.processUpdate
-);
-
-router.post("/update-password",
-  authMiddleware.requireLogin,
-  accountController.validatePassword,
-  accountController.processPasswordChange
-);
+// Protected routes (require login)
+router.get("/", accountController.buildAccountManagement);
+router.get("/update", accountController.buildUpdateAccount);
+router.post("/update", accountController.processUpdate);
+router.post("/update-password", accountController.processPasswordChange);
 
 // Logout route
 router.get("/logout", accountController.logout);
